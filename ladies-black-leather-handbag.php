de92155d8dc0881dc51bee321475ba53f0728108
chandrakantpaliwal <chnadrakantpaliwal@chnadras-air.home>
2014-08-08 18:56:55 +0530

    CB13

--- ladies-black-leather-handbag.php
+++ ladies-black-leather-handbag.php
@@ -92,28 +92,13 @@
 							<div class="choose-size-tab">
 								<p>Available Sizes</p>
 								<ul class="nav nav-tabs" id="myTab2">
-									<li class="active"><a data-toggle="tab" href="#size-xl">9' x 11'</a></li>
-									<li><a data-toggle="tab" href="#size-l">9' x 11'</a></li>
-									<li><a data-toggle="tab" href="#size-m">9' x 11'</a></li>
-									<li><a data-toggle="tab" href="#size-s">9' x 11'</a></li>
+									<li class="active"><a data-toggle="tab" href="#size-xl">11' x 15'</a></li>
 								</ul>
 								
 								<div class="tab-content" id="product-sizes">
 									<div id="size-xl" class="tab-pane fade in active">
-										<p>Dimension	 : 9'x11'</p>
+										<p>Dimension	 : 11' x 15'</p>
 										<span><i>Rate</i>	 : 50-60$</span>
-									</div>
-									<div id="size-l" class="tab-pane fade">
-									  <p>Dimension	 : 9'x11'</p>
-										<span><i>Rate</i>	 : 50-60$</span>
-									</div>
-									<div id="size-m" class="tab-pane fade">
-									 <p>Dimension	 : 9'x11'</p>
-										<span><i>Rate</i>	 : 50-60$</span>
-									</div>
-									<div id="size-s" class="tab-pane fade">
-									 <p>Dimension	 : 9'x11'</p>
-									 	<span><i>Rate</i>	 : 50-60$</span>
 									</div>
 								</div>
 							</div>
@@ -133,7 +118,7 @@
 				<div id="tab-photos" class="tab-pane fade active in">
 					<div class="tab-features">
 						<h3>Ladies Black Leather Handbag</h3>
-						<p>Enhance your style quotient by this elegent and exclusive handbags. This is a hand made leather bags. A must have bag that can go with everything in your wardrobe. This bag comes with two compartment and gives you enough room for all your essentials. You can carry it to a party, movie, college, office.</p>
+						<p>Enhance your style quotient by this elegent and exclusive handbags. Strips of leather beautifully stiched gives this bag a unique design. A must have bag that can go with everything in your wardrobe. This bag comes with two compartment and gives you enough room for all your essentials. You can carry it to a party, movie, college, office.</p>
 					  </div>
 				</div>
 				<div id="reviews" class="tab-pane fade">
@@ -142,12 +127,11 @@
 						<ul>
 							<li><span>Type</span><p>Messenger & Shoulder & Ladies</p></li>
 							<li><span>Color</span><p>Black</p></li>
-							<li><span>Sizes</span><p>9' x 11', 10' x 12', 11' x 13', 12' x 13'</p></li>
-							<li><span>Material</span><p>Leather & Steel</p></li>
-							<li><span>Number of Pockets</span><p>Four Pocket with Chain</p></li>
-							<li><span>Number of Compartments</span><p>Three compartments available</p></li>
-							<li><span>Straps</span><p>2 Strap available</p></li>
-							<li><span>Other Body Features</span><p>Laptop Sleeve with Velcro Closure, Main Compartment, Adjustable Straps at Back</p></li>
+							<li><span>Sizes</span><p>11' x 15'</p></li>
+							<li><span>Material</span><p>Buffalo Leather</p></li>
+							<li><span>Number of Pockets</span><p>One Zipper Pocket</p></li>
+							<li><span>Number of Compartments</span><p>One Compartments</p></li>
+							<li><span>Number of Straps</span><p>One Strap</p></li>
 						</ul>
 					</div>
 				</div>
