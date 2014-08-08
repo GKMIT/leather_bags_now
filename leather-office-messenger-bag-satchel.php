3c09489605517e8ef8194d4a79beb53402a39cef
chandrakantpaliwal <chnadrakantpaliwal@Chnadras-MacBook-Air.local>
2014-08-08 22:30:04 +0530

    gz18

--- leather-office-messenger-bag-satchel.php
+++ leather-office-messenger-bag-satchel.php
@@ -101,9 +101,9 @@
 								<p>Available Sizes</p>
 								<ul class="nav nav-tabs" id="myTab2">
 									<li class="active"><a data-toggle="tab" href="#size-xl">9' x 11'</a></li>
-									<li><a data-toggle="tab" href="#size-l">9' x 11'</a></li>
-									<li><a data-toggle="tab" href="#size-m">9' x 11'</a></li>
-									<li><a data-toggle="tab" href="#size-s">9' x 11'</a></li>
+									<li><a data-toggle="tab" href="#size-l">10' x 13'</a></li>
+									<li><a data-toggle="tab" href="#size-m">11' x 15'</a></li>
+									<li><a data-toggle="tab" href="#size-s">12' x 17'</a></li>
 								</ul>
 								
 								<div class="tab-content" id="product-sizes">
@@ -112,15 +112,15 @@
 										<span><i>Rate</i>	 : 50-60$</span>
 									</div>
 									<div id="size-l" class="tab-pane fade">
-									  <p>Dimension	 : 9'x11'</p>
+									  <p>Dimension	 : 10'x13'</p>
 										<span><i>Rate</i>	 : 50-60$</span>
 									</div>
 									<div id="size-m" class="tab-pane fade">
-									 <p>Dimension	 : 9'x11'</p>
+									 <p>Dimension	 : 11'x15'</p>
 										<span><i>Rate</i>	 : 50-60$</span>
 									</div>
 									<div id="size-s" class="tab-pane fade">
-									 <p>Dimension	 : 9'x11'</p>
+									 <p>Dimension	 : 12'x17'</p>
 									 	<span><i>Rate</i>	 : 50-60$</span>
 									</div>
 								</div>
@@ -148,14 +148,13 @@
 					<div class="product-details">
 						<h3>product features</h3>
 						<ul>
-							<li><span>Type</span><p>Messenger & Shoulder & Ladies</p></li>
-							<li><span>Color</span><p>Black</p></li>
-							<li><span>Sizes</span><p>9' x 11', 10' x 12', 11' x 13', 12' x 13'</p></li>
-							<li><span>Material</span><p>Leather & Steel</p></li>
-							<li><span>Number of Pockets</span><p>Four Pocket with Chain</p></li>
-							<li><span>Number of Compartments</span><p>Three compartments available</p></li>
-							<li><span>Straps</span><p>2 Strap available</p></li>
-							<li><span>Other Body Features</span><p>Laptop Sleeve with Velcro Closure, Main Compartment, Adjustable Straps at Back</p></li>
+							<li><span>Type</span><p>Messenger</p></li>
+							<li><span>Color</span><p>Brown</p></li>
+							<li><span>Sizes</span><p>9' x 11', 10' x 13', 11' x 15', 12' x 17'</p></li>
+							<li><span>Material</span><p>Leather & Metal</p></li>
+							<li><span>Number of Pockets</span><p>One Zipped Inside</p></li>
+							<li><span>Number of Compartments</span><p>Two Compartments</p></li>
+							<li><span>Number of Straps</span><p>One Strap</p></li>
 						</ul>
 					</div>
 				</div>
