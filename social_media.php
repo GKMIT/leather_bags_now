2ec34ba7097fd50ab8e5074a849a41ac0d4d4211
chandrakantpaliwal <chnadrakantpaliwal@chnadras-air.home>
2014-09-03 13:59:48 +0530

    social media file

---
+++ social_media.php
@@ -0,0 +1,11 @@
+<p>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0&appId=1460384074242705";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="http://localhost/leather_bags_now/3-in-1-leather-bag-shoulder-bag-backpack-briefcase.php" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+</p>
