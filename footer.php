45ab8e22d321736092d2647c6c38f5485d8cdcd7
chandrakantpaliwal <chnadrakantpaliwal@Chnadras-MacBook-Air.local>
2014-09-03 16:04:24 +0530

    footer facepile

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@
 				  var js, fjs = d.getElementsByTagName(s)[0];
 				  if (d.getElementById(id)) return;
 				  js = d.createElement(s); js.id = id;
-				  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+				  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0&appId=1460384074242705";
 				  fjs.parentNode.insertBefore(js, fjs);
 				}(document, 'script', 'facebook-jssdk'));</script>
 				<div class="fb-like-box" data-href="https://www.facebook.com/leatherbagsno" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
--- social_media.php
+++ social_media.php
@@ -4,7 +4,7 @@
 		  var js, fjs = d.getElementsByTagName(s)[0];
 		  if (d.getElementById(id)) return;
 		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0&appId=1460384074242705";
 		  fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 	</script>
