f5b2e1ff96477c7c2ad7bb2f8424ec4adfbd09c5
chandrakantpaliwal <chnadrakantpaliwal@chnadras-air.home>
2014-09-03 15:13:46 +0530

    facepile on footer

--- footer.php
+++ footer.php
@@ -31,6 +31,15 @@
 						<li><a href="products.php?category=upcoming" title="">Upcoming</a></li>
 					</ul>
 				</div>
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+				<div class="fb-like-box" data-href="https://www.facebook.com/leatherbagsno" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
 			</div>
 			
 			<!-- <div class="col-md-3">
